0fc011b34d3f87b4aad8bf03324cc82424de8bfa
leonardopea <leonardo.pea.1996@gmail.com>
2023-05-22 15:17:16 +0200

    file nuovi

---
+++ esercizio email/script.js
@@ -0,0 +1,33 @@
+//punto 1//
+
+let email = ['leonardo@gmail.com','fabrizio@gmail.com', 'dobbyèunelfo@libero.it','leonardo@hotmail.it','aieie@libero.it','nelpaesedellemeraviglie@alice.it','frodo@contea.com','samwise@contea.com']
+
+//punto 2//
+
+let domanda = prompt('qual è la tua email?')
+
+let email_utente = domanda.toLowerCase()
+
+console.log(email_utente)
+
+
+//punto 3 e 4//
+
+let flag= false
+
+for ( let i=0 ; i < email.length ; i++ ) {
+
+    if ( email_utente === email[i] ) {
+        flag= true;
+    }
+
+}
+
+if ( flag === true) {
+    document.getElementById('email').innerHTML=`la email ${email_utente} è valida`;
+}
+
+else{
+    document.getElementById('email').innerHTML=`la email ${email_utente} non è valida`;
+}
+
